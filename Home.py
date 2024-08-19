a85f3bf6532d336cbb8c2c97df05eabbfb343e42
유민기 <mgyou@nextree.io>
2024-08-19 21:25:30 +0800

    Data Flow?

--- Home.py
+++ Home.py
@@ -1,24 +1,29 @@
-import streamlit as st
-from langchain.prompts import PromptTemplate
+import streamlit as st # when data changed, whole python file will restart
+from datetime import datetime
 
-st.write("hello")
+today = datetime.today().strftime("%H:%M:%S")
 
-st.write([1, 2, 3, 4])
+st.title(today)
 
-st.write({"x": 1})
-
-# st.write(PromptTemplate)
-
-
-p = PromptTemplate.from_template("xxxx")
-
-## magic - just leave it, program will print into screen
-p
-
-st.selectbox(
+model = st.selectbox(
     "Choose your model",
     (
         "GPT-3",
         "GPT-4",
     ),
 )
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+
+name = st.text_input("What is your name?")
+
+st.write(name)
+
+
+
+value = st.slider("temperature", min_value=0.1, max_value=1.0,)
+
+st.write(value)
